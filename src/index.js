c33f700d94e7b3711e183aae4a3eb241219b7866
Henadzi Hirylovich <Gir.gen2019@gmail.com>
2022-02-06 19:44:22 +0300

    add: result file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let dictionary = {};
+
+
+  
+    if (str.length % 2 != 0) {
+      return false;
+    }
+    fillInDictionary(bracketsConfig);
+    const stack = [];
+    for (let symbol of str) {
+      const topSymbolInStack = stack[stack.length - 1];
+      if (topSymbolInStack !== undefined && topSymbolInStack === symbol) {
+        stack.pop();
+      } else {
+        const closedSymbolForCurrent = dictionary[symbol];
+        stack.push(closedSymbolForCurrent);
+      }
+    }
+    return stack.length === 0;
+  
+  
+  
+  
+  function fillInDictionary(bracketsConfig) {
+    let result = {};
+    bracketsConfig.forEach(element => {
+      const openSymbol = element[0];
+      const closeSymbol = element[1];
+      result[openSymbol] = closeSymbol;
+    });
+    dictionary = result;
+  }
 }
